bfbf46a8eca464933f810dbefb6ed7a306797a00
ihsan-salman <ihsan.salmantr@gmail.com>
2021-09-14 08:38:21 +0000

    settings modification

---
+++ Pur_Beurre_Nutella/settings/production.py
@@ -0,0 +1,16 @@
+from . import *
+
+SECRET_KEY = '#+3^0gumy+=ykmr_rx2i2uo1hgzu-4nxj*pm$4^3vx7v-7v#l1'
+DEBUG = False
+ALLOWED_HOSTS = ['192.241.138.251']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'purbeurre',
+        'USER': 'ihsan',
+        'PASSWORD': 'salman57',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
