153299d3015bde0e3f83b5972535ca0a19047cf8
ihsan-salman <ihsan.salmantr@gmail.com>
2022-03-22 14:22:41 +0000

    functional test correction

--- Pur_Beurre_Nutella/mes_aliments/tests/functional_test.py
+++ Pur_Beurre_Nutella/mes_aliments/tests/functional_test.py
@@ -70,3 +70,29 @@ class TestProject(LiveServerTestCase):
         self.browser.find_element_by_id("button").click()
         self.browser.find_element_by_xpath("//img[@id='fav_img']").click()
         self.assertEqual(self.browser.current_url, self.PRODUCT_DETAIL_URL)
+
+    def test_delete_favorite(self):
+        ''' favorite page functional test '''
+        self.browser.get(self.LOGIN_PAGE_URL)
+        self.browser.find_element_by_id(
+            "id_username").send_keys('iii')
+        self.browser.find_element_by_id(
+            "id_password").send_keys('azeqsd00')
+        # Click button event
+        self.browser.find_element_by_id("registration_button").click()
+        time.sleep(2)
+
+        self.browser.find_element_by_id("text_input").send_keys("pizza")
+        self.browser.find_element_by_id("button").click()
+        time.sleep(2)
+        self.browser.find_element_by_id("submt_button2").click()
+        time.sleep(2)
+        self.browser.find_element_by_id("favorite_link").click()
+        time.sleep(5)
+        self.browser.find_element_by_id("dlt_fav_btn").click()
+        time.sleep(1)
+        self.text = self.browser.find_element_by_tag_name("p")
+        print(self.text.get_attribute('innerHTML'))
+        self.assertEqual(self.text.get_attribute('innerHTML'),
+            "Votre liste de Favoris est nulle! Veuillez enregistrer"
+            " des produits dans la base de données pour afficher cette liste.")
