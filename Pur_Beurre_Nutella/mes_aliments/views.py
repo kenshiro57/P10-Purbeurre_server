2e0a2b19ad439f8032e44b55d1d6d0c4003cf314
ihsan-salman <ihsan.salmantr@gmail.com>
2022-03-22 15:15:17 +0000

    functional test modif

--- Pur_Beurre_Nutella/mes_aliments/views.py
+++ Pur_Beurre_Nutella/mes_aliments/views.py
@@ -47,8 +47,6 @@ def product(request):
                 return render(request, 'error_page/404.html', status=404)
             my_product = product_search(search_request)[0]
             substitutes = substitute_search(search_request)
-            for substitute in substitutes:
-                print(substitute.id)
         context = {'product': my_product,
                    'substitutes': substitutes}
     except IndexError:
