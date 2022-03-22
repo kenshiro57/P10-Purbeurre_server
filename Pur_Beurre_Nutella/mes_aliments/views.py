72494257ac69deb82c199261c4b6c9946c014d72
ihsan-salman <ihsan.salmantr@gmail.com>
2022-03-22 19:51:25 +0000

    func test modif

--- Pur_Beurre_Nutella/mes_aliments/views.py
+++ Pur_Beurre_Nutella/mes_aliments/views.py
@@ -82,6 +82,7 @@ def my_favorite(request):
             favorite_product.append(product_fav[0])
             substitute = Product.objects.filter(id=favorite.substitute_id)
             favorite_substitute.append(substitute[0])
+    print(favorite_product, favorite_substitute)
     context = {'favorite_product': favorite_product,
                'favorite_substitute': favorite_substitute}
     return HttpResponse(template.render(context, request=request))
