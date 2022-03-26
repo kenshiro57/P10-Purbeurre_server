35a88f2daf8e05dc87448ae539bd90541ae1bc22
ihsan-salman <ihsan.salmantr@gmail.com>
2022-03-26 10:56:34 +0000

    last change with pep8 style coding

--- Pur_Beurre_Nutella/settings/__init__.py
+++ Pur_Beurre_Nutella/settings/__init__.py
@@ -39,7 +39,7 @@ SECRET_KEY = 'django-insecure-z$^qa_%66-$i8kd3)dw0=&8dt1_tsi%+=zo)kd36m!bzty7_xu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["192.241.138.251", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.241.138.251", "127.0.0.1", "favarche.fr"]
 
 
 # Application definition
